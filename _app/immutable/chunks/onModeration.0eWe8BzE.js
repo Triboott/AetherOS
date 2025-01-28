9cc0c0421741be9e36abe30d8407d58cdb0fe9f0
Triboot <helicopter.jorge@gmail.com>
2025-01-28 19:18:02 +0100

    first commit

---
+++ _app/immutable/chunks/onModeration.0eWe8BzE.js
@@ -0,0 +1,46 @@
+import{s as U,e as i,a as l,c as a,w as s,g as r,m as K,i as n,n as N,f as o}from"./scheduler.DunZcn5O.js";import{S as X,i as Z}from"./index.-fy_H3D9.js";function $(Q){let m,q=`Hyprland’s moderation and the state of the community has been a touchy subject
+for a while. Today, I would like to address this comprehensively. While I
+believe the community has improved significantly, I also acknowledge that it is
+not yet perfect. This post covers previously raised pain points alongside my
+personal comments, our response, and the future direction of the Hyprland
+community.`,C,p,z=`This will be a somewhat unusual post, as it is both a heart-to-heart message
+aimed at improving the community and a status report. Let me begin by
+highlighting the steps we’ve taken so far, along with our efforts to improve
+moderation. You may or may not agree with these actions, but please understand
+that they were taken in good faith, with the genuine intention of bettering the
+community.`,I,h,F="Previous Attempts",_,c,O=`Some time ago, I requested that Vaxry appoint me as the “Community Manager” to
+help share the burden of feedback and criticism that Vaxry personally receives.
+My goal was to address issues from a different perspective and bring more
+balance to the moderation process. This has certainly been… an experience for
+me, but unfortunately, it has not been as effective as I initially hoped.`,k,d,Y=`Although we came a very long way, my attempts to communicate on or resolve
+existing issues have failed time and time again, which is why I think the
+general narrative around the Hyprland community is wildly inaccurate: the feedback comes in
+the form of rants, and never a constructive, two sided process of communication.`,H,y,j=`From the perspective of someone who actively engages with the community, I must
+say I do not see any instances of problems present in most common comments found
+online, especially the accusation that Hyprland community hosts nazis and
+transphobes. Let me go on to say that this is <em>wildly</em> incorrect as for the
+longest time those who spread hateful ideology, such as, but not limited to
+the previously mentioned groups, have been dealt with swiftly. Those groups of
+people are NOT tolerated, and they will NEVER be tolerated. Let me make that
+<strong>very</strong> clear.`,P,f,E=`Moving on to more visible issues, a frequently raised concern is the lack of
+communication from the moderation team. This is true. Although we have a
+relatively large moderation team, only a few members are consistently active,
+while the rest are barely engaged. As a result, I often find myself the primary
+recipient of complaints, and I acknowledge that I have not been able to handle
+them all effectively. In some cases, I have been insufferable and unresponsive.
+For that, I apologize, and promise to be more approachable in future
+communications regarding moderation.`,T,v,B=`Similarly, I can only apologize for my inability to resolve all the issues
+within the Hyprland community. That is why I am asking you -whoever you may be-
+to talk to me. If you feel harassed or attacked as part of the community, please
+reach out. You can use the mod-mail feature we implemented some time ago, or
+contact me directly via Matrix or email. I am open to discussing any and all
+concerns you might have about the community.`,M,x,D=`For those wanting to address Vaxry’s behavior, that is not my place, nor is it
+the purpose of this post. Vaxry is his own person, and I will not comment on his
+actions. However, Vaxry has asked me to tell you that you can contact him
+directly if you have concerns. He is open to civil discussions.`,A,u,R="Next Steps",L,w,W=`As we look toward improving moderation, I invite all of you to reach out with
+any concerns. You can contact me via email or, preferably, on Matrix at
+<code>@raf:notashelf.dev</code>. In the coming days, we will be hosting a form to collect
+your <strong>anonymous</strong> comments and critiques on moderation. If you’re part of the
+Discord community, I encourage you to participate in the voting process.`,S,g,G=`Additionally, I will be working on reworking the moderation team and improving
+our communication methods. If you have any suggestions, please don’t hesitate to
+let me know through the channels mentioned above.`,V,b,J="Best, raf";return{c(){m=i("p"),m.textContent=q,C=l(),p=i("p"),p.textContent=z,I=l(),h=i("h2"),h.textContent=F,_=l(),c=i("p"),c.textContent=O,k=l(),d=i("p"),d.textContent=Y,H=l(),y=i("p"),y.innerHTML=j,P=l(),f=i("p"),f.textContent=E,T=l(),v=i("p"),v.textContent=B,M=l(),x=i("p"),x.textContent=D,A=l(),u=i("h2"),u.textContent=R,L=l(),w=i("p"),w.innerHTML=W,S=l(),g=i("p"),g.textContent=G,V=l(),b=i("p"),b.textContent=J,this.h()},l(e){m=a(e,"P",{"data-svelte-h":!0}),s(m)!=="svelte-10gmucr"&&(m.textContent=q),C=r(e),p=a(e,"P",{"data-svelte-h":!0}),s(p)!=="svelte-1fmr5ym"&&(p.textContent=z),I=r(e),h=a(e,"H2",{id:!0,"data-svelte-h":!0}),s(h)!=="svelte-1hdpa01"&&(h.textContent=F),_=r(e),c=a(e,"P",{"data-svelte-h":!0}),s(c)!=="svelte-1plwedg"&&(c.textContent=O),k=r(e),d=a(e,"P",{"data-svelte-h":!0}),s(d)!=="svelte-gzx6s5"&&(d.textContent=Y),H=r(e),y=a(e,"P",{"data-svelte-h":!0}),s(y)!=="svelte-91f12i"&&(y.innerHTML=j),P=r(e),f=a(e,"P",{"data-svelte-h":!0}),s(f)!=="svelte-1pyf65o"&&(f.textContent=E),T=r(e),v=a(e,"P",{"data-svelte-h":!0}),s(v)!=="svelte-imltjc"&&(v.textContent=B),M=r(e),x=a(e,"P",{"data-svelte-h":!0}),s(x)!=="svelte-nn1aav"&&(x.textContent=D),A=r(e),u=a(e,"H2",{id:!0,"data-svelte-h":!0}),s(u)!=="svelte-1cfb4gb"&&(u.textContent=R),L=r(e),w=a(e,"P",{"data-svelte-h":!0}),s(w)!=="svelte-ukmpwu"&&(w.innerHTML=W),S=r(e),g=a(e,"P",{"data-svelte-h":!0}),s(g)!=="svelte-1fxyvfv"&&(g.textContent=G),V=r(e),b=a(e,"P",{"data-svelte-h":!0}),s(b)!=="svelte-1g32ydr"&&(b.textContent=J),this.h()},h(){K(h,"id","previous-attempts"),K(u,"id","next-steps")},m(e,t){n(e,m,t),n(e,C,t),n(e,p,t),n(e,I,t),n(e,h,t),n(e,_,t),n(e,c,t),n(e,k,t),n(e,d,t),n(e,H,t),n(e,y,t),n(e,P,t),n(e,f,t),n(e,T,t),n(e,v,t),n(e,M,t),n(e,x,t),n(e,A,t),n(e,u,t),n(e,L,t),n(e,w,t),n(e,S,t),n(e,g,t),n(e,V,t),n(e,b,t)},p:N,i:N,o:N,d(e){e&&(o(m),o(C),o(p),o(I),o(h),o(_),o(c),o(k),o(d),o(H),o(y),o(P),o(f),o(T),o(v),o(M),o(x),o(A),o(u),o(L),o(w),o(S),o(g),o(V),o(b))}}}const ne={title:"On Hyprland Moderation",date:1728487e3,author:{name:"NotAShelf",link:"https://github.com/NotAShelf/",picture:"/imgs/profile_pictures/raf.webp"}};class oe extends X{constructor(m){super(),Z(this,m,null,$,U,{})}}export{oe as default,ne as metadata};
---
+++ _app/immutable/entry/start.CXZ3hp_W.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.m_-qn8w_.js";export{t as start};
